cf88ee10511ef2d52f0da1f0ae5b20a51a93603b
KITrue <KeepInventory23@gmail.com>
2023-09-02 15:42:38 -0300

    Update settings.py

--- projeto/settings.py
+++ projeto/settings.py
@@ -124,20 +124,20 @@ USE_I18N = True
 USE_TZ = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.static(os.path.dirname(__file__)
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL='/static/'
-STATICFILES_DIRS=(),
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (),
+STATIC_ROOT = BASE_DIR / 'static'
 
 
-MEDIA_URL="img/"
-MEDIA_ROOT=BASE_DIR/'media'
+MEDIA_URL = "img/"
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
