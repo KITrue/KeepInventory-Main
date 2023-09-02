c043fb5cbd6e264b86db11ceec9d9fecf7754c40
KITrue <KeepInventory23@gmail.com>
2023-09-02 10:02:45 -0300

    banco de dados conexão
    eu quero chorar

---
+++ projeto/db.py
@@ -0,0 +1,10 @@
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'neondb',
+    'USER': 'KITrue',
+    'PASSWORD': 'JHA4Bp3GPRoS',
+    'HOST': 'ep-twilight-cherry-94612651.us-east-2.aws.neon.tech',
+    'PORT': '5432',
+  }
+}
--- projeto/settings.py
+++ projeto/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+from .db import *  # noqa
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
