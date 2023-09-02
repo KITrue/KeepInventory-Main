1b56f8bf75dcd14ff5ece4541f1fa50956d19520
KITrue <KeepInventory23@gmail.com>
2023-09-02 16:16:11 -0300

    Projeto uploads incriveis

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include
 
 
@@ -24,6 +22,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # recipes teste
     path('', include('gerencia.urls'))  # Main-Home
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
