e54fb5909e5c947df5d2b0e2b8e9c1507746a1da
KITrue <KeepInventory23@gmail.com>
2023-09-11 00:28:19 -0300

    Just commit

--- projeto/settings.py
+++ projeto/settings.py
@@ -1,6 +1,5 @@
 
 from pathlib import Path
-import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -98,7 +97,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'gerencia/static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type
