2bbd86dc5dc34b3d9a795b347aa7729240542dc0
KITrue <KeepInventory23@gmail.com>
2023-09-11 01:52:37 -0300

    tentativa

--- projeto/settings.py
+++ projeto/settings.py
@@ -97,7 +97,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles/static'
+STATICFILES_DIR = [
+    BASE_DIR / 'staticfiles/static'
+]
 
 
 # Default primary key field type
