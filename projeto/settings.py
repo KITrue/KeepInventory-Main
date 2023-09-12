0c31690f04f423ac0bc85131651794c86240935f
KITrue <KeepInventory23@gmail.com>
2023-09-12 20:20:08 -0300

    Update settings.py

--- projeto/settings.py
+++ projeto/settings.py
@@ -56,11 +56,11 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'neondb',
-        'USER': 'KITrue',
-        'PASSWORD': 'JHA4Bp3GPRoS',
-        'HOST': 'ep-twilight-cherry-94612651.us-east-2.aws.neon.tech',
-        'PORT': '5432',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'gyEkP0Av1gRzqlLTnhCk',
+        'HOST': 'containers-us-west-129.railway.app',
+        'PORT': '7726',
     },
 
 
