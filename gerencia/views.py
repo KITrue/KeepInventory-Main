5b023e96721f72bc4337b7d38fd8f66523caaf11
KITrue <140995642+KITrue@users.noreply.github.com>
2023-09-12 21:59:13 -0300

    nao sei mais

--- gerencia/views.py
+++ gerencia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from .models import Produtos
 from .api.serializers import ProdutosSerializer
@@ -32,22 +32,22 @@ def saidas(request):
     
 
 def cadastro(request):
+    if request.method == 'POST':
+        # Criar um novo objeto Produto com os dados do formulário
+        novo_produto = Produtos()
+        novo_produto.nome = request.POST.get('nome')
+        novo_produto.marca = request.POST.get('marca')
+        novo_produto.quantidade = request.POST.get('quantidade')
+        novo_produto.descricao = request.POST.get('descricao')
+        novo_produto.preco = request.POST.get('preco')
+        novo_produto.save()
+
+        # Redirecionar para a página de sucesso ou qualquer outra página desejada
+        return redirect('.')
+
+    # Se a requisição não for POST, apenas renderize o formulário
     return render(request, 'gerencia/pages/cadastro.html')
-    novo_produto = Produtos()
-    # Salvando novo produto para banco de dados
-    
-    novo_produto.nome = request.POST('nome')
-    novo_produto.marca = request.POST('marca')
-    novo_produto.total = request.POST('total')
-    novo_produto.descricao = request.POST('descricao')
-    novo_produto.preco = request.POST('preco')
-    novo_produto.save()
 
-    #exibir todos os usuariosjá cadastrados em uma nova página
-    produtos = {
-        'produtos': Produtos.objects.all()
-    
-    }
-    return render(request, 'gerencia/pages/cadastro.html', produtos)
-    
-
+# Se desejar, você pode criar uma página de sucesso após o cadastro
+def pagina_sucesso(request):
+    return render(request, 'gerencia/pages/entradas.html')
