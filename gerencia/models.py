b68167a332a9216869ad5c20b8299c2570495b81
KITrue <KeepInventory23@gmail.com>
2023-09-17 19:51:19 -0300

    Id field

--- gerencia/models.py
+++ gerencia/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Produtos(models.Model):
-    id = models.BigAutoField(
+    id = models.AutoField(
         primary_key=True
     )
     nome = models.CharField(
