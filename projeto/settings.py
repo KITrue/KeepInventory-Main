78556b0a00486b749c2afb7a58eb20bfb18ace61
KITrue <KeepInventory23@gmail.com>
2023-09-17 22:58:12 -0300

    dale?

--- projeto/settings.py
+++ projeto/settings.py
@@ -101,13 +101,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'gerencia/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
-MIDIA_URL = '/gerencia/static/gerencia/images'
-MIDIA_ROOT = BASE_DIR / 'gerencia/static/gerencia/images'
+MIDIA_URL = '/static/'
+MIDIA_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
